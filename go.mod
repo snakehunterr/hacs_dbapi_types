1e58199936281f6775e247dc3636074d0b6b64b6
sn1k3hunt3r <sn1k3hunt3r@gmail.com>
2025-03-11 21:37:22 +0300

    git init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/snakehunterr/hacs_dbapi_types
+
+go 1.24.0
---
+++ types.go
@@ -0,0 +1,30 @@
+package types
+
+import "time"
+
+type Client struct {
+	ID      uint   `json:"client_id"`
+	Name    string `json:"client_name"`
+	IsAdmin bool   `json:"is_admin"`
+}
+
+type Expense struct {
+	ID     uint      `json:"expense_id"`
+	Date   time.Time `json:"expense_date"`
+	Amount float64   `json:"expense_amount"`
+}
+
+type Room struct {
+	ID          uint    `json:"room_id"`
+	ClientID    uint    `json:"client_id"`
+	PeopleCount uint8   `json:"room_people_count"`
+	Area        float64 `json:"room_area"`
+}
+
+type Payment struct {
+	ID       uint      `json:"payment_id"`
+	ClientID uint      `json:"client_id"`
+	RoomID   uint      `json:"room_id"`
+	Date     time.Time `json:"payment_date"`
+	Amount   float64   `json:"payment_amount"`
+}
