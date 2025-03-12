c2fd1fa13186b283bd24c6c32d65936603fec0e1
sn1k3hunt3r <sn1k3hunt3r@gmail.com>
2025-03-12 17:09:17 +0300

    added last_edited field to all structs

--- types.go
+++ types.go
@@ -5,30 +5,34 @@ import (
 )
 
 type Client struct {
-	ID      uint   `json:"client_id"`
-	Name    string `json:"client_name"`
-	IsAdmin bool   `json:"is_admin"`
+	ID         uint      `json:"client_id"`
+	Name       string    `json:"client_name"`
+	IsAdmin    bool      `json:"is_admin"`
+	LastEdited time.Time `json:"last_edited"`
 }
 
 type Expense struct {
-	ID     uint      `json:"expense_id"`
-	Date   time.Time `json:"expense_date"`
-	Amount float64   `json:"expense_amount"`
+	ID         uint      `json:"expense_id"`
+	Date       time.Time `json:"expense_date"`
+	Amount     float64   `json:"expense_amount"`
+	LastEdited time.Time `json:"last_edited"`
 }
 
 type Room struct {
-	ID          uint    `json:"room_id"`
-	ClientID    uint    `json:"client_id"`
-	PeopleCount uint8   `json:"room_people_count"`
-	Area        float64 `json:"room_area"`
+	ID          uint      `json:"room_id"`
+	ClientID    uint      `json:"client_id"`
+	PeopleCount uint8     `json:"room_people_count"`
+	Area        float64   `json:"room_area"`
+	LastEdited  time.Time `json:"last_edited"`
 }
 
 type Payment struct {
-	ID       uint      `json:"payment_id"`
-	ClientID uint      `json:"client_id"`
-	RoomID   uint      `json:"room_id"`
-	Date     time.Time `json:"payment_date"`
-	Amount   float64   `json:"payment_amount"`
+	ID         uint      `json:"payment_id"`
+	ClientID   uint      `json:"client_id"`
+	RoomID     uint      `json:"room_id"`
+	Date       time.Time `json:"payment_date"`
+	Amount     float64   `json:"payment_amount"`
+	LastEdited time.Time `json:"last_edited"`
 }
 
 type APIErrorCode byte
